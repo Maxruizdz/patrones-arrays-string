5a14bd45837c40d3a60cb0d740a979da377b49f2
Maxruizdz <maxruizdz@hotmail.com>
2024-01-18 17:03:54 -0300

    Resolucion-Search 2D Array Matrix

--- Busqueda_binaria/Program.cs
+++ Busqueda_binaria/Program.cs
@@ -1,64 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// See https://aka.ms/net
 using System.ComponentModel.Design;
 
-int[] nums = { 4, 5, 6, 7, 0, 1, 2,3};
+int[,] matriz = {
+     { 1, 3, 5, 7},
+    { 10, 11, 16,20},
+    { 23, 30, 34 ,60}
+};
+
 int objetivo = 3;
-int numero =buscar_numero_Array_Rotado(nums, objetivo);
-
-Console.WriteLine(numero);
 
 
+bool encontre= buscar_numero_Array(matriz, objetivo);
+Console.WriteLine(encontre);
+
+static bool buscar_numero_Array(int[,] matriz, int objetivo) {
+
+    int puntero_izquierdo = 0;
+    int columna_matriz= 0;
+    int puntero_derecho = matriz.GetLength(1) - 1;
+    while (puntero_izquierdo<=puntero_derecho ) {
 
 
-
-static int buscar_numero_Array_Rotado(int[] nums, int numero_buscar) {
-    int cont = 0;
-        int puntero_izquierdo = 0;
-        int puntero_derecho= nums.Length - 1;
-
-    while (puntero_izquierdo <= puntero_derecho)
-    {
-        cont++;
-        int puntero_intermedio =  puntero_izquierdo + (puntero_derecho -puntero_izquierdo) / 2;
-        if (nums[puntero_intermedio] == numero_buscar)
+        
+       int valor_ultimo_elemento_de_fila = matriz[columna_matriz, puntero_derecho];
+        
+     
+        if (valor_ultimo_elemento_de_fila >= objetivo)
         {
-            return puntero_intermedio;
-        }
-        if (nums[puntero_intermedio] > nums[puntero_izquierdo])
-        {
-            if (nums[puntero_izquierdo] <= numero_buscar && nums[puntero_intermedio] >= numero_buscar)
+
+            int punto_intermedio = puntero_izquierdo + (puntero_derecho - puntero_izquierdo) / 2;
+            if (matriz[columna_matriz, punto_intermedio] == objetivo)
+            {
+             
+                return true;
+                
+
+            }
+
+            if (matriz[columna_matriz, puntero_derecho] > objetivo && objetivo <= matriz[columna_matriz, punto_intermedio])
             {
 
-                puntero_derecho = puntero_intermedio - 1;
+                puntero_derecho = punto_intermedio - 1;
 
             }
             else
             {
-                puntero_izquierdo = puntero_intermedio + 1;
+
+                puntero_izquierdo = punto_intermedio + 1;
             }
+
         }
         else {
 
-            if (nums[puntero_izquierdo] <numero_buscar)
+            if (columna_matriz == matriz.GetLength(0)-1)
             {
-
-                puntero_izquierdo++;
+                puntero_izquierdo = puntero_derecho + 1;
 
             }
-            else { 
-            
-            puntero_izquierdo--;
-            }
-        
+            else { columna_matriz++; }
         
         }
-
-        Console.WriteLine(cont);
-
-    }
+           
 
 
+        
+        
 
-    return -1;
-    }
+        }
+
+
+
+
+    return false;
+
+
+
+}
+
+
+
+
+
+
