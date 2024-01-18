0b921e79d498454ba248358ef4485b96cacc3969
Maxruizdz <maxruizdz@hotmail.com>
2024-01-18 17:21:18 -0300

    --amend

--- Busqueda_binaria/Program.cs
+++ Busqueda_binaria/Program.cs
@@ -25,15 +25,27 @@ static bool buscar_numero_Array(int[,] matriz, int objetivo) {
        int valor_ultimo_elemento_de_fila = matriz[columna_matriz, puntero_derecho];
         
      
-        if (valor_ultimo_elemento_de_fila >= objetivo)
+        if (valor_ultimo_elemento_de_fila < objetivo)
         {
+            if (columna_matriz == matriz.GetLength(0) - 1)
+            {
+                puntero_izquierdo = puntero_derecho + 1;
+
+            }
+            else { columna_matriz++; }
+
+
+        }
+        else {
+
+
 
             int punto_intermedio = puntero_izquierdo + (puntero_derecho - puntero_izquierdo) / 2;
             if (matriz[columna_matriz, punto_intermedio] == objetivo)
             {
-             
+
                 return true;
-                
+
 
             }
 
@@ -49,16 +61,6 @@ static bool buscar_numero_Array(int[,] matriz, int objetivo) {
                 puntero_izquierdo = punto_intermedio + 1;
             }
 
-        }
-        else {
-
-            if (columna_matriz == matriz.GetLength(0)-1)
-            {
-                puntero_izquierdo = puntero_derecho + 1;
-
-            }
-            else { columna_matriz++; }
-        
         }
            
 
